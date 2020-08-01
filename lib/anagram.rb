39f91251cfe0e3534da1e3efa0b5ae840de61f3c
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-01 19:45:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    collection = []
+   array.each do |x|
+   collection << x if x.split("").sort == @word.split("").sort
+    end
+    collection
+  end
+  
+end   
